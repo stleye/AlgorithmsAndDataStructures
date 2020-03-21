d42d1c74a4720c20b325a56d7b8b631812e1f00a
Sebastian Tleye <stleye@ovloop.com>
2020-03-20 21:36:09 -0300

    Replaced project with framework

---
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructures.h
@@ -0,0 +1,19 @@
+//
+//  AlgorithmsAndDataStructures.h
+//  AlgorithmsAndDataStructures
+//
+//  Created by Sebastian Tleye on 20/03/2020.
+//  Copyright © 2020 HumileAnts. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlgorithmsAndDataStructures.
+FOUNDATION_EXPORT double AlgorithmsAndDataStructuresVersionNumber;
+
+//! Project version string for AlgorithmsAndDataStructures.
+FOUNDATION_EXPORT const unsigned char AlgorithmsAndDataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlgorithmsAndDataStructures/PublicHeader.h>
+
+
